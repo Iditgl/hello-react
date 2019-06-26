05006594ecf147d6c25b78a40dddbf7b37507fee
Iditgl <46907722+Iditgl@users.noreply.github.com>
2019-06-26 16:21:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+'use strict'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const express = require('express')
+const Prometheus = require('prom-client')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const app = express()
+const port = process.env.PORT || 3000
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const checkoutsTotal = new Prometheus.Counter({
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
+})
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
+
+// Runs before each requests
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now()
+  next()
+})
+
+app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    res.json({ message: 'Hello World!' })
+    next()
+  }, Math.round(Math.random() * 200))
+})
+
+app.get('/bad', (req, res, next) => {
+  next(new Error('My Error'))
+})
+
+app.get('/checkout', (req, res, next) => {
+  const paymentMethod = Math.round(Math.random()) === 0 ? 'stripe' : 'paypal'
+
+  checkoutsTotal.inc({
+    payment_method: paymentMethod
+  })
+
+  res.json({ status: 'ok' })
+  next()
+})
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.statusCode = 500
+  // Do not expose your error in production
+  res.json({ error: err.message })
+  next()
+})
+
+// Runs after each requests
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.route.path, res.statusCode)
+    .observe(responseTimeInMs)
+
+  next()
+})
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  clearInterval(metricsInterval)
+
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    process.exit(0)
+  })
+})
